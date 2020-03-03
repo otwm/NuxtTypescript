bd0393238f304a44bb45e609de147d31ca5bba98
dong o kim <otwmoo@gmail.com>
2020-03-03 22:35:10 +0900

    경로 추가

---
+++ pages/index3.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    ok ! 3
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import { Person } from "~/types";
+import Card from "~/components/Card.vue"
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State people: Person
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
